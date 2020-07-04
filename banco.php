282af65490702706f611c8b7d5c10002c26ab66f
David Almeida <davidalmeidadrive@gmail.com>
2020-07-04 02:41:54 -0300

    construct and static

--- banco.php
+++ banco.php
@@ -2,7 +2,17 @@
 
 require_once 'src/Conta.php';
 
-$primeiraConta = new Conta();
-$primeiraConta->saca(200);
+$primeiraConta = new Conta('123.456.789-11', 'Fulano de tal');
+$primeiraConta->deposita(500);
+$primeiraConta->saca(300);
 
-echo $primeiraConta->recuperaSaldo();
+
+echo $primeiraConta->recuperaNomeTitular() . PHP_EOL;
+echo $primeiraConta->recuperaCpfTitular() . PHP_EOL;
+echo $primeiraConta->recuperaSaldo() . PHP_EOL;
+
+
+$segundaConta = new Conta('987.654.321-12', 'Patricia');
+var_dump($segundaConta);
+
+echo Conta::$recuperaNumeroDeContas();
--- src/Conta.php
+++ src/Conta.php
@@ -5,7 +5,28 @@ class Conta
 
     private string $cpfTitular;
     private string $nomeTitular;
-    private float $saldo = 0;
+    private float $saldo;
+    private static $numeroDeContas = 0;
+
+    public function __construct(string $cpfTitular, string $nomeTitular)
+    {
+        $this->validaNomeTitular($nomeTitular);
+        $this->nomeTitular = $nomeTitular;
+        $this->cpfTitular = $cpfTitular;
+        $this->saldo = 0;
+
+        self::$numeroDeContas++;
+    }
+
+    public function defineNomeTitular(string $nome): void
+    {
+        $this->nomeTitular = $nome;
+    }
+
+    public function defineCpfTitular(string $cpf): void
+    {
+        $this->cpfTitular = $cpf;
+    }
 
     public function recuperaSaldo(): float
     {
@@ -49,4 +70,20 @@ class Conta
             $contaDestino->deposita($valorTransferir);
         }
     }
+
+    private function validaNomeTitular(string $nomeTitular)
+    {
+        if (strlen($nomeTitular) < 5) {
+            echo "Nome precisa ter pelo menos 5 caracteres";
+            exit();
+        }
+    }
+
+
+    public static function recuperaNumeroDeContas() : int
+    {
+        return self::$numeroDeContas;
+    }
+
+
 }
